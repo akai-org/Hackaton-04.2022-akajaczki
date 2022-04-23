6db9a441e09cb79a20781ce8a2e666f8964678c5
mozzar <realmozzar@gmail.com>
2022-04-23 13:19:06 +0200

    api - kategorie, ciekawostki,produkty

---
+++ pages/api/products/[id].ts
@@ -0,0 +1,31 @@
+import {NextApiRequest, NextApiResponse} from 'next';
+import nc from 'next-connect';
+import {prisma} from '@/prisma';
+
+const handler = nc<NextApiRequest, NextApiResponse>()
+    //get selected
+    .get(async (req, res) => {
+        const {id} = req.query;
+        const result = await prisma.products.findFirst({where: {id: Number(id)}})
+        res.json(result);
+    })
+    //update product
+    .put(async (req, res) => {
+        const {id} = req.query;
+        const {name, barcode, harmfulness, categoriesId} = req.body;
+        const result = await prisma.products.update({
+            where: {id: Number(id)},
+            data: {name, barcode, harmfulness, categoriesId}
+        })
+        res.json(result);
+    })
+    //delete product
+    .delete(async (req, res) => {
+        const {id} = req.query;
+        const result = await prisma.products.delete({
+            where: {id: Number(id)}
+        });
+        res.json(result);
+    });
+
+export default handler;
