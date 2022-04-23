a9edbe68e41c28c61e6128cc2bd2b2b5af2f700c
Krychaxp <krychaxp@op.pl>
2022-04-23 13:28:51 +0200

    fix

--- pages/api/categories/[id].ts
+++ pages/api/categories/[id].ts
@@ -1,30 +1,21 @@
-import {NextApiRequest, NextApiResponse} from 'next';
+import { NextApiRequest, NextApiResponse } from 'next';
 import nc from 'next-connect';
-import {prisma} from '@/prisma';
-
+import { prisma } from '@/prisma';
 
 const handler = nc<NextApiRequest, NextApiResponse>()
-    //get products by category
-    .get(async (req, res) => {
+	.get(async (req, res) => {
+		const { id } = req.query;
+		const result = await prisma.products.findMany({
+			where: { categoriesId: Number(id) },
+		});
+		res.json(result);
+	})
+	.delete(async (req, res) => {
+		const { id } = req.query;
 
-        const {id} = req.query;
-        const result = await prisma.products.findMany(
-            {
-                where: {categoriesId: Number(id)}
+		const result = await prisma.categories.delete({ where: { id: Number(id) } });
 
-            })
-        res.json(result);
-
-    })
-
-    //delete category, but first delete products related
-    .delete(async (req, res) => {
-
-        const {id} = req.query;
-        
-        const result = await prisma.categories.delete({where: {id: Number(id)}});
-
-        res.json(result);
-    });
+		res.json(result);
+	});
 
 export default handler;
